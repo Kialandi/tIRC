29f7c59c062210f58ea3f4db8e4abbf077f72ac9
David <worleyd@purdue.edu>
2016-12-04 00:16:31 -0500

    dear lord we finally reached the second page

--- public/index.js
+++ public/index.js
@@ -1,12 +1,15 @@
+
 $(document).ready(function(){
   var user,pass;
   $("#login-submit").click(function(){
     user=$("#login-username").val();
     pass=$("#login-password").val();
     $.post("http://localhost:8080/login",{user: user,password: pass}, function(data){
-      if(data==='done')
+      if(data === 'yes')
       {
-        alert("login success");
+        
+        window.location.replace("http://localhost:8080/chat.html");
+        //     alert("login successsadasd");
       }
     });
   });
